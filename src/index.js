a024219aa833b154a2e7326c808890fea45df8f8
Fiodor Furick <fff24.06.91@gmail.com>
2021-01-20 00:30:06 +0200

    1st time use hooks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState}  from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+
+    const [color, setColor] = useState('gray');
+    const [fntSize, setFontSize] = useState(14)
+
     return (
-        <h1>Hi</h1>
+        <div style={{
+            padding: '10px',
+            background: color,
+            fontSize: `${fntSize}px`
+        }}>
+            <h1>Hello World</h1>
+            <button onClick={() => setColor('white')}>
+                Light
+            </button>
+            <button onClick={() => setColor('gray')}>
+                Dark
+            </button>
+            <button onClick={() => setFontSize((s) => s+2)} >
+                +
+            </button>
+        </div>
     )
 }
 
